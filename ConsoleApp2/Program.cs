da2c5a38f5b26c32d33f8af3208a66d982a72693
Usuario <Usuario@CPD03-M09>
2024-04-30 11:09:34 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                string[] nome = { "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
+
+                Console.WriteLine("Digite o numero do mês desejado: ");
+
+                int i = int.Parse(Console.ReadLine());
+
+                int numeromes = i - 1;
+
+                Console.WriteLine("O nome selecionado foi: " + nome[numeromes]);
+
+                Console.ReadKey();
+        }
+    }
+}
